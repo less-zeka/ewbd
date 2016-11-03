7a8d60846947b229e13b82a53aed79a9f3d9e563
less <info@open-source.tips>
2016-11-03 10:32:37 +0100

    MainMenu

--- Ewbd/Assets/Ground.cs
+++ Ewbd/Assets/Ground.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 
-public class Ground : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		var scaleX = 10.050f;
-		var scaleY = 10.050f;
-		transform.GetComponent<Renderer>().material.mainTextureScale = new Vector2(scaleX , scaleY );
-
-	}
+public class Ground : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        //TODO?!
+        var scaleX = 10.050f;
+        var scaleY = 10.050f;
+        transform.GetComponent<Renderer>().material.mainTextureScale = new Vector2(scaleX, scaleY);
+    }
 }
---
+++ Ewbd/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect((Screen.width / 2) - 100, (Screen.height / 2) - 100, 150, 100));
+
+        var myButtonStyle = new GUIStyle(GUI.skin.button) {fontSize = 50};
+        if (GUILayout.Button("Play", myButtonStyle))
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        GUILayout.EndArea();
+    }
+}
